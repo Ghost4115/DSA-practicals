fea1243d9208f124e3e0ee8bf51d4f7e3d2dec8a
Ghost4115 <manavuttekar2@gmail.com>
2025-05-03 13:44:29 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,3 +1,10 @@
+#Name:Manav Mangesh Uttekar
+#Roll no.: 71
+#Problem statement: A book consists of chapters, chapters consist of
+#sections and sections consist of subsections.Construct a tree and print
+#the nodes. Find the time and space requirements of your method.
+
+
 # Node class representing each element (book, chapter, section, subsection)
 class TreeNode:
     def __init__(self, name):
