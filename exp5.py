67b889fd78c29f1f702bf765c2ea1272f080ed50
Ghost4115 <manavuttekar2@gmail.com>
2025-05-03 13:43:00 +0530

    Update exp5.py

--- exp5.py
+++ exp5.py
@@ -1,3 +1,15 @@
+Name:Manav Mangesh Uttekar
+Roll no.: 71
+Problem statement: Beginning with an empty binary search tree, Construct
+binary search tree by inserting the values in the order given. After
+constructing a binary tree -
+i. Insert new node
+ii. Find number of nodes in longest path from root
+iii. Minimum data value found in the tree,
+iv. Change a tree so that the roles of the left and right pointers are
+swapped at every node
+v. Search a value
+
 # Define a Node class for the binary search tree (BST)
 class Node:
     def __init__(self, data):
